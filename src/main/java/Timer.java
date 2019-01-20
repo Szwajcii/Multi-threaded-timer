f8c178b4c7f8f8d4128d951bcc45884d5ce442b9
Szwajcii <d.szwajkos@gmail.com>
2019-01-20 12:43:58 +0100

    Change field type in Timer object and implement thread id via constructor.

--- src/main/java/Timer.java
+++ src/main/java/Timer.java
@@ -5,14 +5,15 @@ public class Timer  implements Runnable{
     private String name;
     private int id;
     private static int threadId = 0;
-    private Long startTime;
-    private Long endTime;
+    private long startTime;
+    private long endTime;
     private long pasue = 0;
     private volatile boolean isThreadRun = true;
 
 
     public Timer(String name) {
         this.name = name;
+        this.id = this.threadId++;
     }
 
 
