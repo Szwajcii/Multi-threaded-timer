17c3455572a08cac59a71fcfa9b859d05d620bda
Szwajcii <d.szwajkos@gmail.com>
2019-01-20 12:41:43 +0100

    Implement methods to get command and value from console input.

--- src/main/java/InputParser.java
+++ src/main/java/InputParser.java
@@ -1,5 +1,25 @@
+import java.util.InputMismatchException;
+
 public class InputParser {
 
+    public static String getCommand(String input){
 
+        String[] command = input.split(" ");
+        return command[0].trim();
+    }
+
+
+    public static String getValue(String input) {
+        String[] command = input.split(" ");
+
+        if(command.length > 1){
+            return command[1].trim();
+        }
+
+        else {
+            throw new InputMismatchException("Wrong input!");
+        }
+
+    }
 
 }
--- src/main/java/ThreadTimerHandler.java
+++ src/main/java/ThreadTimerHandler.java
@@ -1,6 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class ThreadTimerHandler {
 
+    private final List<Thread> threadList = new ArrayList<>();
+    private final List<Timer> timerList = new ArrayList<>();
 
+    public void start() {
+
+        boolean isRunning = true;
+        Scanner scanner = new Scanner(System.in);
+
+
+
+    }
 
 
 }
